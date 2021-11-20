98c49cb8cc4a3a226e372f7b9b897836cd90e0cd
Matéo HORGUES <rezomatho@gmx.com>
2021-11-20 15:14:51 +0100

    Modification de "import pygame as p" en "import pygame as pg"

--- src/pnj.py
+++ src/pnj.py
@@ -1,8 +1,8 @@
-import pygame as p
+import pygame as pg
 
 class Pnj:
     def __init__(self,sprite,num):
-        self.sprite = p.image.load(sprite)
+        self.sprite = pg.image.load(sprite)
         with open(f'../res/text/dialogues/pnj{num}.txt','r') as a:
             self.dial = a.readline().strip().split("__")
             print(self.dial)
