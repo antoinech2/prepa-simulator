2d4eb21583a9fc62647b620d4e80e4c395163596
Ordralfabetix <rezomatho@gmx.com>
2021-11-24 21:11:09 +0100

    style: Correction de la syntaxe d'Adam

--- src/text.py
+++ src/text.py
@@ -1,15 +1,13 @@
 import pygame as pg
 
 
-
-class dialogue():
-
-    def __init__(self,game):
+class Dialogue():
+    def __init__(self, game):
         talk_box_surf = pg.image.load("res/textures/talk_box_next.png")
-        talk_box_x = int(talk_box_surf.get_width()*1.5)
+        talk_box_x = int(talk_box_surf.get_width()*1.5)  # FIXME C'est degueulasse d'utiliser int()
         talk_box_y = int(talk_box_surf.get_height()*1.5)
         self.talk_box_img = pg.transform.scale(talk_box_surf,(talk_box_x,talk_box_y))
         self.game = game
 
-    def show_talk_box(self,x,y):
-        self.game.screen.blit(self.talk_box_img,(x,y))
+    def show_talk_box(self, x, y):
+        self.game.screen.blit(self.talk_box_img, (x, y))
