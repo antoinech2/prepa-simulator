a9f1242699b8ed8385ceef17997f68960705137b
antoinech <42295406+antoinech2@users.noreply.github.com>
2021-12-12 02:16:36 +0100

    Connventionnalisation

--- src/dialogue.py
+++ src/dialogue.py
@@ -14,23 +14,22 @@ import numpy as np
 
 class Dialogue():
     DATABASE_LOCATION = "res/text/dialogues.db"
+    IMAGE_LOCATION = "res/textures/talk_box_next.png"
     def __init__(self, game, npc):
         self.game = game
 
         #Graphique
-        self.talk_box_surf = pg.image.load(
-            "res/textures/talk_box_next.png").convert()
-        # FIXME Passage en relatif
+        self.talk_box_surf = pg.image.load(self.IMAGE_LOCATION).convert()
         self.talk_box_x = int(self.talk_box_surf.get_width()*0.75)
         self.talk_box_y = int(self.talk_box_surf.get_height()*0.75)
-        self.talk_box_img = pg.transform.scale(
-            self.talk_box_surf, (self.talk_box_x, self.talk_box_y))
+        self.talk_box_img = pg.transform.scale(self.talk_box_surf, (self.talk_box_x, self.talk_box_y))
         self.talk_box_img.set_colorkey([255, 255, 255])
 
         #BDD
         self.db_connexion = sql.connect(self.DATABASE_LOCATION)
         self.db_cursor = self.db_connexion.cursor()
 
+        #Gestion de l'affichage partiel du texte
         self.lettre_cooldown = 5
         self.current_npc = npc
         self.current_text = ""  # text actuel
@@ -50,18 +49,21 @@ class Dialogue():
         self.new_line()
 
     def close(self):
+        """Fermeture du dialogue"""
         self.db_cursor.close()
         self.db_connexion.close()
         self.game.player.is_talking = False
         self.game.dialogue = None
 
-    def update(self):  # cette fonction s'execute à chaques ticks
+    def update(self):
+        """Fonction de mise à jour générale"""
         self.show_talk_box()  # on affiche limage de la boite de dialgue
         if self.is_writing:
             if self.game.tick_count % self.lettre_cooldown == 0:
                 self.new_letter()
 
     def next_dialogue(self):
+        """Passe au dialogue suivant lorsque le joueur presse la touche"""
         if self.is_writing:
             self.is_writing = False
             self.current_letter_id = -1
@@ -69,7 +71,8 @@ class Dialogue():
         else:
             self.new_line()
 
-    def new_line(self):  # on passe au dialogue suivant
+    def new_line(self):
+        """Passe à la ligne suivante du dialogue"""
         # on "efface" le dialogue precedent
         self.talk_box_img = pg.transform.scale(
             self.talk_box_surf, (self.talk_box_x, self.talk_box_y))
@@ -83,8 +86,8 @@ class Dialogue():
         else:
             self.close()
 
-    def new_letter(self):  # cette fonction ecrit une lettre de plus à chaques fois qu'elle est appelée
-        # meme principe que dans dial_suiv mais avec des lettres
+    def new_letter(self):
+        """Affiche une nouvelle lettre du texte"""
         if self.current_letter_id < len(self.current_text) - 1:
             self.current_letter_id += 1
             self.ecrire(self.current_text[:self.current_letter_id+1], 30, 100)
@@ -93,13 +96,12 @@ class Dialogue():
             self.current_letter_id = -1
             self.is_writing = False
 
-    def ecrire(self, texte, x, y, color=(0, 0, 0)):  # cette fonction affiche un text
+    def ecrire(self, texte, x, y, color=(0, 0, 0)):
+        """Affiche le texte sur le cadre de dialogue"""
         text_affiche = self.font.render(texte, False, color)
         self.talk_box_img.blit(text_affiche, (x, y))
 
-    def show_talk_box(self):  # cette fonction affiche l'image de la boite de dialgue
-        a = self.game.screen.get_size()[0]/2
-        b = self.game.screen.get_size()[1]
-        c = self.talk_box_img.get_width()/2
-        d = self.talk_box_img.get_height()
-        self.game.screen.blit(self.talk_box_img, (a-c, b-d))
+    def show_talk_box(self):
+        """Affichage graphique de la boîte"""
+        rect = self.talk_box_img.get_rect(center=(self.game.screen.get_size()[0]/2, self.game.screen.get_size()[1]-self.talk_box_img.get_height()/2))
+        self.game.screen.blit(self.talk_box_img, rect)
