e85781683958e4a101bab228827d5786a1bea1fb
fmkr-project <ugo58956@protonmail.com>
2021-12-18 23:43:10 +0100

    Passage des touches en constantes
    Je suggère d'enlever Ctrl droit en tant que touche de sprint (j'ai pas cette touche sur mon clavier), de toute façon une seule touche suffit

--- src/inputs.py
+++ src/inputs.py
@@ -1,21 +1,22 @@
 
 import pygame as pg
+import controls as ctrl
 
 def handle_pressed_key(game):
     """Transmet toutes les touches préssées"""
     pressed = pg.key.get_pressed() #Gestion des touches du clavier
     # On envoie le statut des 4 touches de déplacement pour être traité
-    game.player.move([pressed[pg.K_UP], pressed[pg.K_RIGHT], pressed[pg.K_DOWN], pressed[pg.K_LEFT]], pressed[pg.K_RCTRL] or pressed[pg.K_LCTRL])
+    game.player.move([pressed[ctrl.PLAYER_MOVE_UP], pressed[ctrl.PLAYER_MOVE_RIGHT], pressed[ctrl.PLAYER_MOVE_DOWN], pressed[ctrl.PLAYER_MOVE_LEFT]], pressed[ctrl.PLAYER_SPRINT] or pressed[ctrl.PLAYER_SPRINT_SEC])
 
 def handle_key_down_event(game, event):
     """Transmet l'évènement d'une toucher qui vient d'être pressée"""
-    if event.key == pg.K_SPACE:  # si Espace est pressée
+    if event.key == ctrl.ACTION_INTERACT:  # si Espace est pressée
         if game.dialogue == None:
             game.map_manager.object_manager.pickup_check()
             game.map_manager.npc_manager.check_talk()
         else:
             game.dialogue.next_dialogue()
-    elif event.key == pg.K_F11:
+    elif event.key == ctrl.GAME_FULLSCREEN:
         game.change_window_size(fullscreen = (not game.is_fullscreen))
-    elif event.key == pg.K_ESCAPE:
+    elif event.key == ctrl.GAME_TERMINATE:
         game.is_running = False
