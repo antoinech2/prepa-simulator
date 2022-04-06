ef947ce2bc1571a4f43f8a7e601ae0a3a2ad2da3
fmkr-project <ugo58956@protonmail.com>
2022-04-06 14:30:56 +0200

    Update scripts.py

--- src/scripts.py
+++ src/scripts.py
@@ -68,7 +68,7 @@ def object():
 
 def quiz():
     """Script qui lance le mini-jeu quiz"""
-    return(["""ran(60, 68)""",
+    return(["""ran(1, 68)""",
             """launchmgm("select", self.acc)"""])
 
 
