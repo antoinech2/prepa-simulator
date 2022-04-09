5b4d244dbc3e41b3176899c42947085b46b05832
fmkr-project <ugo58956@protonmail.com>
2022-04-09 14:18:45 +0200

    Lot de questions n°5
    Au programme : enfin 100 questions ! Méca (fin) : moment cinétique, mouvement dans un cfc, rappels (ch. Méca 0), méca en ref non galiléen.
    Pas de frottements solides car c'est juste de la méca classique avec une ou deux forces liées aux lois de Coulomb.

--- src/scripts.py
+++ src/scripts.py
@@ -68,7 +68,7 @@ def object():
 
 def quiz():
     """Script qui lance le mini-jeu quiz"""
-    return(["""ran(1, 98)""",
+    return(["""ran(1, 112)""",
             """launchmgm("select", self.acc)"""])
 
 
