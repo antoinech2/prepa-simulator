ae0ddc1b0932bc64578404b94639e0295b9364a6
fmkr-project <ugo58956@protonmail.com>
2022-04-14 16:02:56 +0200

    Lot de questions n°9
    Au programme : vraiment très peu de questions. Quantique et probas

--- src/scripts.py
+++ src/scripts.py
@@ -68,7 +68,7 @@ def object():
 
 def quiz():
     """Script qui lance le mini-jeu quiz"""
-    return(["""ran(1, 161)""",
+    return(["""ran(1, 164)""",
             """launchmgm("select", self.acc)"""])
 
 
